28afbe8914a42d03bd308ebfdd71ab71f57366bb
Vedant Jhawar <vedantj1203@gmail.com>
2025-12-20 12:42:16 -0500

    full detection piepeline working

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import cv2 as cv
 import numpy as np
 from pyorbbecsdk import *
 from utils import frame_to_bgr_image
-from math import pi, radians, isfinite, tan
+from math import pi, sqrt
 from sys import exit
 from ultralytics import YOLO
 
@@ -11,18 +11,7 @@ MIN_DEPTH = 20  # 20mm
 MAX_DEPTH = 10000  # 10000mm = 10 m
 PRINT_INTERVAL = 1
 AVG_INTERVAL = 30
-YOLO_INTERVAL = 20# Depth FOV
-HFOV_DEPTH_DEG = 91.0
-VFOV_DEPTH_DEG = 66.0
-# RGB/Color FOV
-HFOV_COLOR_DEG = 86.0
-VFOV_COLOR_DEG = 55.0
-
-# Precompute radians
-HFOV_DEPTH_RAD = radians(HFOV_DEPTH_DEG)
-VFOV_DEPTH_RAD = radians(VFOV_DEPTH_DEG)
-HFOV_COLOR_RAD = radians(HFOV_COLOR_DEG)
-VFOV_COLOR_RAD = radians(VFOV_COLOR_DEG)
+YOLO_INTERVAL = 20
 
 model = YOLO("yolov10m.pt") 
 
@@ -43,8 +32,6 @@ dark_red2  = (179, 255, 255)
 
 light_yellow = (25, 90, 60)
 dark_yellow = (32, 255, 255)
-
-circles = [] # (x, y, r)
 
 
 # computes circularity of shape; returns True if its >thresh (1 is perfect circle)
@@ -125,9 +112,33 @@ def segmentation(mask_code, image_stream=None, image_path='', bounding_box_color
 
 def main():
     pipeline = Pipeline()
-
-    pipeline.start()
+    config = Config()
+    
     print("Pipeline started successfully. Press 'q' or ESC to exit.")
+
+    profile_list = pipeline.get_stream_profile_list(OBSensorType.COLOR_SENSOR)
+    color_profile = profile_list.get_default_video_stream_profile()
+    config.enable_stream(color_profile)
+    profile_list = pipeline.get_stream_profile_list(OBSensorType.DEPTH_SENSOR)
+    depth_profile = profile_list.get_default_video_stream_profile()
+    config.enable_stream(depth_profile)
+
+    depth_intrinsics = depth_profile.as_video_stream_profile().get_intrinsic()
+    extrinsic = depth_profile.get_extrinsic_to(color_profile)
+
+    try:
+        pipeline.enable_frame_sync()
+    except Exception as e:
+        print(e)
+
+    try:
+        pipeline.start(config)
+    except Exception as e:
+        print(e)
+        return
+
+    align_filter = AlignFilter(align_to_stream=OBStreamType.COLOR_STREAM)
+
 
     # Set window size
     window_width = 1280
@@ -135,17 +146,20 @@ def main():
     cv.namedWindow("QuickStart Viewer", cv.WINDOW_NORMAL)
     cv.resizeWindow("QuickStart Viewer", window_width, window_height)
 
-    i = 0
-    center_distance_avg = 0
-    distances = [0] * AVG_INTERVAL
-
     frameCount =  0
+    landmarks = None
 
     while True:
         try:
             frames = pipeline.wait_for_frames(100)
             if frames is None:
                 continue
+
+            frames = align_filter.process(frames)
+            if not frames:
+                continue
+
+            frames  = frames.as_frame_set()
 
             frameCount += 1
 
@@ -166,45 +180,56 @@ def main():
             
             color_image, circle = segmentation('ry', image_stream=color_image, bounding_box_color=(0, 0, 0))
 
-            if circle and frameCount % YOLO_INTERVAL == 0:
-                # crop out circle
-                x, y, r = circle
-                raw_copy = cv.circle(raw, (x, y), r, (0, 0, 0), -1)
-                yolo_inference(raw_copy, circle)
-                frameCount = 0
-
+            if circle is None:
+                landmarks = None
+        
             # Process depth data
             width = depth_frame.get_width()
             height = depth_frame.get_height()
             scale = depth_frame.get_depth_scale() 
 
-            
             depth_data = np.frombuffer(depth_frame.get_data(), dtype=np.uint16).reshape((height, width))
-            depth_data = depth_data.astype(np.float32) * scale
             depth_data = np.where((depth_data > MIN_DEPTH) & (depth_data < MAX_DEPTH), depth_data, 0).astype(np.uint16)
+            
+
+            if circle and frameCount % YOLO_INTERVAL == 0:
+                # crop out circle
+                x, y, r = circle
+                raw_copy = cv.circle(raw, (x, y), r, (0, 0, 0), -1)
+                #print(f"Depth_data: {depth_data}")
+                landmarks = yolo_inference(raw_copy, circle, depth_data, depth_intrinsics, extrinsic)
+                frameCount = 0
 
             # Create depth visualization
             depth_image = cv.normalize(depth_data, None, 0, 255, cv.NORM_MINMAX, dtype=cv.CV_8U)
             depth_image = cv.applyColorMap(depth_image, cv.COLORMAP_JET)
+            depth_image = cv.addWeighted(color_image, 0.5, depth_image, 0.5, 0)
 
-            center_y = int(height / 2)
-            center_x = int(width / 2)
-            center_distance = depth_data[center_y, center_x]
-            distances[i % AVG_INTERVAL] = center_distance
-            i += 1
-
-            valid = [d for d in distances if d > 0]
-
-            if valid:
-                center_distance_avg = sum(valid) / len(valid)
 
             # Resize and combine images
             color_image_resized = cv.resize(color_image, (window_width // 2, window_height))
             depth_image_resized = cv.resize(depth_image, (window_width // 2, window_height))
             combined_image = np.hstack((color_image_resized, depth_image_resized))
 
-            cv.putText(combined_image, f"Center distance: {round(center_distance_avg / 10, 2)} cm", (30, 30), cv.FONT_HERSHEY_PLAIN, 2, color=(255, 0, 0), thickness=3)
-            
+            if (depth_frame.get_width() != color_frame.get_width() or depth_frame.get_height() != color_frame.get_height()):
+                print("WARNING: depth and color not same size after alignment!")
+
+            landmark_format = "No landmarks detected"
+
+            if landmarks and len(landmarks) > 0:
+                landmark_format = ""                
+                for key in landmarks.keys():
+                    landmark_format += str(key) + " : " + str(landmarks[key]) + " meters\n"
+
+                landmark_format = landmark_format.split("\n")
+                i = 0
+
+                for line in landmark_format:
+                    cv.putText(combined_image, line, (30, 30 + 30 * i), cv.FONT_HERSHEY_PLAIN, 2, color=(255, 0, 0), thickness=3)
+                    i += 1
+            else:
+                cv.putText(combined_image, landmark_format, (30, 30), cv.FONT_HERSHEY_PLAIN, 2, color=(255, 0, 0), thickness=3)
+
             cv.imshow("QuickStart Viewer", combined_image)
 
             if cv.waitKey(1) in [ord('q'), ESC_KEY]:
@@ -218,31 +243,18 @@ def main():
     exit()
 
 
-def pixel_to_camera_xyz(dx, dy, depth_m, d_w, d_h):
-    """
-    Map depth pixel (dx, dy, depth_m) to camera coordinates (X, Y, Z),
-    using the DEPTH FOV.
-    """
-    if depth_m is None or depth_m <= 0 or not isfinite(depth_m):
-        return None
+def get_distance(point1, point2):
+    x1, y1, z1 = point1.x, point1.y, point1.z
+    x2, y2, z2 = point2.x, point2.y, point2.z
 
-    cx_d = d_w / 2.0
-    cy_d = d_h / 2.0
+    dx = x2 - x1
+    dy = y2 - y1
+    dz = z2 - z1
 
-    nx = (dx - cx_d) / (d_w / 2.0)
-    ny = (dy - cy_d) / (d_h / 2.0)
-
-    theta_x = nx * (HFOV_DEPTH_RAD / 2.0)
-    theta_y = ny * (VFOV_DEPTH_RAD / 2.0)
-
-    X = depth_m * tan(theta_x)
-    Y = depth_m * tan(theta_y)
-    Z = depth_m
-
-    return np.array([X, Y, Z], dtype=np.float32)
+    return round(sqrt(dx * dx + dy * dy + dz * dz), 3)
 
 
-def yolo_inference(image_array, circle):    
+def yolo_inference(image_array, circle, depth_matrix, depth_intrinsics, extrinsic):    
     results = model(image_array)
     
     # get x and y coordinate of landmark
@@ -251,17 +263,35 @@ def yolo_inference(image_array, circle):
     result = results[0]
     noBoxes = result.boxes is None or len(result.boxes) == 0
 
+    landmarks = {}
+
     if not noBoxes:
         for box in result.boxes: # each box is an actual objects 
             pos = box.xyxy[0].cpu().numpy()
             box_x_avg = int((pos[0] + pos[2]) / 2)
             box_y_avg = int((pos[1] + pos[3]) / 2)
 
-            print(f"Object position: {box_x_avg.item(), box_y_avg.item()} Circle location: {circle[0], circle[1]} ")
+            target_x_pixel, target_y_pixel, _ = circle
 
-        annotated_frame = result.plot()
-        cv.imshow("YOLO11 Detection", annotated_frame)
+            depth_target = depth_matrix[target_y_pixel, target_x_pixel]
+            depth_landmark = depth_matrix[box_y_avg, box_x_avg]
 
+            if depth_target == 0 or depth_landmark == 0:
+                continue
+
+            target_realworld = transformation2dto3d(OBPoint2f(target_x_pixel, target_y_pixel), depth_target, depth_intrinsics, extrinsic)
+            landmark_realworld = transformation2dto3d(OBPoint2f(box_x_avg, box_y_avg), depth_landmark, depth_intrinsics, extrinsic)
+
+            name = model.names[int(box.cls)]
+
+            distance = get_distance(target_realworld, landmark_realworld)
+            #print(f"Object name: {name} Distance (mm): {distance}")
+            landmarks[str(name)] = round(distance / 1000, 3)
+
+        #annotated_frame = result.plot()
+        #cv.imshow("YOLO11 Detection", annotated_frame)
+
+    return landmarks
 
 
 if __name__ == "__main__":
